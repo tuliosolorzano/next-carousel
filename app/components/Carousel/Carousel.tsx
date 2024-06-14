8c40a015f01f7e3bc0ebee2f1a4296b7ff26384e
Tulio Solorzano <tulio@tulio.us>
2024-06-14 14:27:30 -0700

    Removing Next/Head for now due to warning
    Removing Next/Head for now due to warning

--- app/components/Carousel/Carousel.tsx
+++ app/components/Carousel/Carousel.tsx
@@ -1,4 +1,5 @@
 'use client';
+import React from 'react';
 
 import styled, { keyframes, css } from 'styled-components';
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,5 @@
-// import Image from "next/image";
-import Head from 'next/head';
 import { Carousel } from '@/app/components/Carousel';
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Next.js Carousel</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <Carousel />
-    </>
-  );
+  return <Carousel />;
 }
