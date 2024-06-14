bd8f0661692221c5fe1a5aba73b1c31db9c234d3
Tulio Solorzano <tulio@tulio.us>
2024-06-14 13:53:05 -0700

    Adding CSS and Layouts
    Adding CSS and Layouts

--- app/components-layout/Footer/Footer.tsx
+++ app/components-layout/Footer/Footer.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
-  )
-}
+    <footer>
+      <div className='footer-content'> Footer</div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- app/components-layout/Header/Header.tsx
+++ app/components-layout/Header/Header.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Header = () => {
   return (
-    <header>Header</header>
-  )
-}
+    <header>
+      <div className='nav-content'>Header</div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
